2ffe9b59604a1cc3679743bc72d07b69b05cbc1c
steeven_js <jacques.steeven@gmail.com>
2024-09-09 19:14:51 -0400

    chore: Update contact form layout and message handling

--- resources/views/mail/contact.blade.php
+++ resources/views/mail/contact.blade.php
@@ -1,17 +1,51 @@
 <x-mail::message>
-# Introduction
+# Nouveau message de contact
 
-## Contact Information
-- **Company:** {{ $contactMail->company }}
-- **First Name:** {{ $contactMail->firstName }}
-- **Last Name:** {{ $contactMail->lastName }}
-- **Email:** {{ $contactMail->email }}
-- **Phone Number:** {{ $contactMail->phoneNumber }}
+## Informations de contact
+<x-mail::table>
+| Champ | Valeur |
+| ----- | ------ |
+| Entreprise | {{ e($contactMail->company) }} |
+| Prénom | {{ e($contactMail->firstName) }} |
+| Nom | {{ e($contactMail->lastName) }} |
+| Email | {{ e($contactMail->email) }} |
+| Téléphone | {{ e($contactMail->phoneNumber) ?: 'Non fourni' }} |
+| Site web | {{ e($contactMail->website) ?: 'Non fourni' }} |
+</x-mail::table>
 
 @if ($contactMail->message)
 ## Message
-{{ $contactMail->message }}
+<x-mail::panel>
+{{ e($contactMail->message) }}
+</x-mail::panel>
 @endif
+
+@if (!empty($contactMail->services))
+## Services demandés
+<x-mail::table>
+| Services |
+| -------- |
+@foreach(json_decode($contactMail->services, true) as $service)
+| {{ e($service) }} |
+@endforeach
+</x-mail::table>
+@endif
+
+@if (!empty($contactMail->budget))
+## Budget
+<x-mail::table>
+| Min | Max |
+| --- | --- |
+@php
+    $budget = json_decode($contactMail->budget, true);
+@endphp
+| {{ number_format($budget[0], 0, ',', ' ') }} € | {{ number_format($budget[1], 0, ',', ' ') }} € |
+</x-mail::table>
+@endif
+
+<x-mail::subcopy>
+Ce message a été généré automatiquement. Veuillez ne pas y répondre directement.
+</x-mail::subcopy>
 
 Merci,<br>
 {{ config('app.name') }}
