976fa50aae62fe41acd551740b311150ed0d412f
steeven_js <jacques.steeven@gmail.com>
2024-09-09 13:58:43 -0400

    reset

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,8 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Admin Test',
-            'email' => 'admin@test.com',
+            'name' => 'Test User',
+            'email' => 'test@example.com',
         ]);
     }
 }
